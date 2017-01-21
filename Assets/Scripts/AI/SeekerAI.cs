bbe85765423735f251a4a5cd67274395e41ccdf4
ZHIMIN ZHAO <zhimin@seas.upenn.edu>
2017-01-21 12:48:34 -0800

    reimport from AI_Branch

--- Assets/Scripts/AI/SeekerAI.cs
+++ Assets/Scripts/AI/SeekerAI.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.AI;
 using UnityEngine;
 
 public class SeekerAI : EnemyAI
@@ -13,33 +14,44 @@ public class SeekerAI : EnemyAI
 
     public SeekerState curState;
 
-    public float proximityRadius;
-    float defaultRadius; //Assign value
+    float attackDamage = 20f;
+    float proximityRadius = 15f;
+    float defaultRadius = 5f; //Assign value
     float lightRadius; //Assign value
+    float playerDist;
 
-	// Use this for initialization
-	void Start ()
+    Vector3 playerPosition;
+    NavMeshAgent agent;
+    GameObject player;
+
+    // Use this for initialization
+    void Start ()
     {
+        player = GameObject.FindWithTag("Player");
         curState = SeekerState.IDLE;
-	}
+        agent = GetComponent<NavMeshAgent>();
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
+        playerPosition = player.transform.position;
+        playerDist = (playerPosition - transform.position).magnitude;
+
         if (curHealth <= 0)
         {
-            curState = SeekerState.DEATH;
-            Destroy(this);
+            curState = SeekerState.DEATH;//then play the death animation
+            Destroy(this, 1f);
         }
 
 		if (curState == SeekerState.IDLE)
         {
             Wander();
         }
-        if (curState == SeekerState.ATTACK)
+        /*if (curState == SeekerState.ATTACK)
         {
             Attack();
-        }
+        }*/
         
 	}
 
@@ -47,20 +59,42 @@ public class SeekerAI : EnemyAI
     {
         //put navmesh to go from left to right
         //find distance between Player and enemy
-        // if (dist <= proximity radius) then curState = SeekerState.ATTACK
+        if (playerDist <= proximityRadius)
+        {
+            //curState = SeekerState.ATTACK;
+            agent.destination = playerPosition;
+            return;
+        }
+        else {
+            Vector3 randomDirection = UnityEngine.Random.insideUnitSphere * defaultRadius;
+            randomDirection += transform.position;
+
+            NavMeshHit navHit;
+            NavMesh.SamplePosition(randomDirection, out navHit, defaultRadius, NavMesh.AllAreas);
+            agent.destination = navHit.position;
+        }
     }
 
-    void Attack()
+    IEnumerator Damage()
     {
-        //NavMesh
-        // if (dist > proximity radius) then curState = SeekerState.IDLE
+        PlayerCharacter.pc.LoseHealthPoint(attackDamage * Time.deltaTime);
+        yield return null;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            curState = SeekerState.ATTACK;//then play the attack animation
+            StartCoroutine(Damage());
+        }
     }
 
     void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.tag == "Particle")
         {
-            TakeDamage(5.0f/*other.gameObject.particleDamage*/);
+            TakeDamage(5.0f);//Or other.gameObject.particleDamage
         }
     }
 }
