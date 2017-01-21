75d379386d79e1fc6c6aa6fac04918a772bc7731
Prateek Marathe <prateekmarathe594@gmail.com>
2017-01-21 00:29:52 -0800

    powerup abstract class

---
+++ Assets/Scripts/Powerups.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Powerups : MonoBehaviour {
+
+	protected virtual void Awake()
+    {
+
+    }
+
+    protected abstract void OnCollisionEnter(Collision Collide);
+
+}
