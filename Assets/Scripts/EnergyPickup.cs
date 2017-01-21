eb7007815d6ca94a5941abaebb365c71badc7821
ZHIMIN ZHAO <zhimin@seas.upenn.edu>
2017-01-21 01:25:39 -0800

    powerups template

---
+++ Assets/Scripts/EnergyPickup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyPickup : Powerups {
+    protected override void Awake()
+    {
+
+    }
+
+    protected override void OnCollisionEnter(Collision collision)
+    {
+
+    }
+}
