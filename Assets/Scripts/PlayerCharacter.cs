74eb55d9472374708f77c0a6b8749460a1c21190
Chunze <cchunze@yahoo.com>
2017-01-21 01:26:15 -0800

    Added secondary fire

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -8,20 +8,28 @@ public class PlayerCharacter : MonoBehaviour {
     // For shooting purposes
     public GameObject particleShot;
     public float shotDistance = 100;
+
+	private SecondaryFire _secondaryFire;
     
 	// Use this for initialization
 	void Start () {
-		
+		_secondaryFire = GetComponentInChildren<SecondaryFire> ();
 	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (CrossPlatformInputManager.GetButtonDown("Fire1"))
-        {
-            Shoot();
-        }
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetButtonDown("Fire1"))
+		{
+			Shoot();
+		}
+
+		if (Input.GetButtonDown("Fire2"))
+		{
+			_secondaryFire.Fire();
+			Debug.Log("secondary fire!");
+		}
+	}
 
     void Shoot()
     {
