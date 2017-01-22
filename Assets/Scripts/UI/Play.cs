57c7fd33b78df3cc64a63227173c64fa50898cc9
ZHIMIN ZHAO <zhimin@seas.upenn.edu>
2017-01-21 20:26:01 -0800

    UI main menu update

---
+++ Assets/Scripts/UI/Play.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Play : MonoBehaviour
+{
+    public GameObject mainMenu;
+
+    public void Begin()
+    {
+        mainMenu.SetActive(false);
+    }
+}
