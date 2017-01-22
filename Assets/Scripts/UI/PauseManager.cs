e87c63aa6e8ebd642cbbf1aaba59d0a4c16a8582
Zhimin Zhao <Zhimin Zhao>
2017-01-21 23:59:30 -0800

    pause menu tested

---
+++ Assets/Scripts/UI/PauseManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour {
+
+    Canvas canvas;
+
+    void Start()
+    {
+        canvas = GetComponent<Canvas>();
+        canvas.enabled = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause();
+        }
+    }
+
+    public void Pause()
+    {
+        canvas.enabled = !canvas.enabled;
+        Time.timeScale = Time.timeScale == 0 ? 1 : 0;
+    }
+}
